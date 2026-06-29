53c2e2dd1b387c6ef239a36f928b5d2d7aff4c80
avneeshm <avneeshm@godrej.com>
2026-06-29 12:05:41 +0530

    Avneesh Mishra Code for Practice

---
+++ Pandas..py
@@ -0,0 +1,14 @@
+# Action          Command
+# Install         pip install notebook
+# Run             jupyter notebook
+# Verify Version  jupyter --version
+
+# Topics :
+# 1 Creating DataFrame
+# 2 Dealing with Rows & columns
+# 3 Operations : min,max,std,describe
+# 4 conditional Selection
+# 5 Set index
+
+# Process of cleaning messy data is called Data Munging or Data Wrangling
+
